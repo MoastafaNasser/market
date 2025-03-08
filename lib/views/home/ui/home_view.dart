5c966c1a3c27a9be39a4706ea75c2e8e523b3057
MostafaNasser <https://github.com/MoastafaNasser>
2025-03-08 21:16:32 +0200

    refactor for categories list

--- lib/views/home/ui/home_view.dart
+++ lib/views/home/ui/home_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:marketapp/core/app_colors.dart';
 
 import 'package:marketapp/core/components/custom_Search_Field.dart';
+import 'package:marketapp/views/home/ui/widgets/Category_list.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
@@ -23,73 +24,10 @@ class HomeView extends StatelessWidget {
           SizedBox(
             height: 20,
           ),
-          SizedBox(
-            height: 100,
-            child: ListView.builder(
-              scrollDirection: Axis.horizontal,
-              itemCount: categories.length,
-              itemBuilder: (context, index) {
-                return Padding(
-                  padding: const EdgeInsets.all(10),
-                  child: Column(
-                    children: [
-                      CircleAvatar(
-                        radius: 30,
-                        backgroundColor: AppColors.kPrimaryColor,
-                        foregroundColor: AppColors.kWhiteColor,
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Icon(categories[index].iconData, size: 40),
-                            
-                          ],
-                        ),
-                      ),
-                      
-                      Text(categories[index].text),
-
-                    ],
-                  ),
-                );
-              },
-            ),
-          ),
+          categoriesList(),
         ],
       ),
     );
   }
 }
 
-List<Category> categories = [
-  Category(
-    iconData: Icons.sports,
-    text: "Sports",
-  ),
-  Category(
-    iconData: Icons.tv,
-    text: "Electronics",
-  ),
-  Category(
-    iconData: Icons.collections,
-    text: "collections",
-  ),
-  Category(
-    iconData: Icons.games,
-    text: "Games",
-  ),
-  Category(
-    iconData: Icons.book,
-    text: "Books",
-  ),
-  Category(
-    iconData: Icons.bike_scooter,
-    text: "bike",
-  ),
-];
-
-class Category {
-  final IconData iconData;
-  final String text;
-
-  Category({required this.iconData, required this.text});
-}
---
+++ lib/views/home/ui/widgets/Category_list.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:marketapp/core/app_colors.dart';
+
+class categoriesList extends StatelessWidget {
+  const categoriesList({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 100,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: categories.length,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.all(10),
+            child: Column(
+              children: [
+                CircleAvatar(
+                  radius: 30,
+                  backgroundColor: AppColors.kPrimaryColor,
+                  foregroundColor: AppColors.kWhiteColor,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(categories[index].iconData, size: 40),
+                      
+                    ],
+                  ),
+                ),
+                
+                Text(categories[index].text),
+    
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+List<Category> categories = [
+  Category(
+    iconData: Icons.sports,
+    text: "Sports",
+  ),
+  Category(
+    iconData: Icons.tv,
+    text: "Electronics",
+  ),
+  Category(
+    iconData: Icons.collections,
+    text: "collections",
+  ),
+  Category(
+    iconData: Icons.games,
+    text: "Games",
+  ),
+  Category(
+    iconData: Icons.book,
+    text: "Books",
+  ),
+  Category(
+    iconData: Icons.bike_scooter,
+    text: "bike",
+  ),
+];
+
+class Category {
+  final IconData iconData;
+  final String text;
+
+  Category({required this.iconData, required this.text});
+}
