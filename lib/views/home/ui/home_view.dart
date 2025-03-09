38eb8c4b8dac0aa7ab2b35c295e33ca2d4275a10
MostafaNasser <https://github.com/MoastafaNasser>
2025-03-09 17:18:34 +0200

    finish product card ui

--- lib/views/home/ui/home_view.dart
+++ lib/views/home/ui/home_view.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:marketapp/core/app_colors.dart';
-
 import 'package:marketapp/core/components/custom_Search_Field.dart';
+import 'package:marketapp/views/auth/ui/widgets/Custom_E_Button.dart';
 import 'package:marketapp/views/home/ui/widgets/Category_list.dart';
 
 class HomeView extends StatelessWidget {
@@ -25,9 +25,104 @@ class HomeView extends StatelessWidget {
             height: 20,
           ),
           categoriesList(),
+          Card(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(15),
+            ),
+            child: Column(
+              children: [
+                Stack(
+                  children: [
+                    ClipRRect(
+                      borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(15),
+                        bottomLeft: Radius.circular(15),
+                        bottomRight: Radius.circular(15),
+                      ),
+                      child: Image(
+                        image: AssetImage(
+                            "assets/images/97d359_15028f250eb24051bde6447c8b4ca51e~mv2.jpg"),
+                      ),
+                    ),
+                    Container(
+                      height: 40,
+                      width: 80,
+                      decoration: BoxDecoration(
+                        color: AppColors.kPrimaryColor,
+                        borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(15),
+                          bottomRight: Radius.circular(15),
+                        ),
+                      ),
+                      child: Center(
+                        child: Text(
+                          "10% OFF",
+                          style: TextStyle(
+                            color: AppColors.kWhiteColor,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 20),
+                Row(
+                  children: [
+                    Text(
+                      "product there",
+                      style: TextStyle(
+                        fontSize: 23,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Spacer(),
+                    IconButton(
+                      icon: Icon(
+                        Icons.favorite,
+                        color: AppColors.kGreyColor,
+                      ),
+                      onPressed: () {},
+                    ),
+                  ],
+                ),
+                SizedBox(height: 20),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 5),
+                  child: Row(
+                    children: [
+                      Column(
+                        children: [
+                          Text(
+                            "223 LE",
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: AppColors.kBlackColor,
+                            ),
+                          ),
+                          Text(
+                            "290 LE",
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: AppColors.kGreyColor,
+                              decoration: TextDecoration.lineThrough,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Spacer(),
+                      CustomEButton(
+                        text: "Buy Now",
+                        onTap: () {},
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
   }
 }
-
