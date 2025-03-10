96779a6d480d56497c73185f5cdc28b9213f94bf
MostafaNasser <https://github.com/MoastafaNasser>
2025-03-10 15:26:40 +0200

    finish Favorite view ui

--- lib/views/home/favorits/ui/favorite_view.dart
+++ lib/views/home/favorits/ui/favorite_view.dart
@@ -1,17 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:marketapp/core/components/prodcut_list.dart';
 
 class FavoriteView extends StatelessWidget {
   const FavoriteView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        "favorite View",
-        style: TextStyle(
-          fontSize: 25,
-          fontWeight: FontWeight.bold,
-        ),
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: ListView(
+        children: [
+          Center(
+            child: Text(
+              "Your Favorite productes",
+              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          productList(),
+        ],
       ),
     );
   }
--- lib/views/home/store/ui/store_view.dart
+++ lib/views/home/store/ui/store_view.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:marketapp/core/components/custom_Search_Field.dart';
 import 'package:marketapp/core/components/prodcut_list.dart';
-import 'package:marketapp/views/home/ui/widgets/Category_list.dart';
+
 
 class storeView extends StatelessWidget {
   const storeView({super.key});
