3d59ddb7b3b354a249b698d4cbd97fc551b11566
MostafaNasser <https://github.com/MoastafaNasser>
2025-03-10 17:06:10 +0200

    create card profile view

--- lib/views/home/profile/ui/profile_view.dart
+++ lib/views/home/profile/ui/profile_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:marketapp/core/app_colors.dart';
 
 class profileView extends StatelessWidget {
   const profileView({super.key});
@@ -6,11 +7,68 @@ class profileView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text(
-        "profile View",
-        style: TextStyle(
-          fontSize: 25,
-          fontWeight: FontWeight.bold,
+      child: Card(
+        margin: EdgeInsets.all(8),
+        color: AppColors.kWhiteColor,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              radius: 45,
+              backgroundColor: AppColors.kPrimaryColor,
+              foregroundColor: AppColors.kWhiteColor,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(
+                    Icons.person,
+                    size: 60,
+                  ),
+                ],
+              ),
+            ),
+            Text(
+              "Uesr name",
+              style: TextStyle(fontSize: 20),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              "Email name",
+              style: TextStyle(fontSize: 20),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Card(
+              color: AppColors.kPrimaryColor,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(16),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(16),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Icon(
+                      Icons.person,
+                      color: AppColors.kWhiteColor,
+                    ),
+                    Text(
+                      "Edit Name",
+                      style: TextStyle(color: AppColors.kWhiteColor),
+                    ),
+                
+                    Icon(Icons.arrow_forward_ios, color: AppColors.kWhiteColor),
+                  ],
+                ),
+              ),
+            )
+          ],
         ),
       ),
     );
